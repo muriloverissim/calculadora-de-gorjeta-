9537b75db8c3c06a699efd80351db934d4f6923e
murilo verissimo <muriloverissimo98@outlook.com>
2026-06-01 00:19:25 -0300

    nono commit do css e terceiro js

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
-alert ("Hello world")
+let conta = 0
+let pessoas = 0
+let porcentagem = 0
+
+const contaInput = document.querySelector("#conta")
+contaInput.addEventListener("input", )
+
+function receberValorConta(evento) {
+   conta = Number(evento.target.value)
+}
+
+const pessoasInput = document.querySelector("#pessoas")
+pessoasInput.addEventListener("input", receberQuantidadePessoas)
+
+function receberQuantidadePessoas(evento) {
+    const paragrafoErro = document.querySelector(".pessoas #erro")
+   const divErro = document.querySelector(".pessoas .input-box")
+   
+    if(evento.target.value === "0") {
+         paragrafoErro.computedStyleMap.display = "block"
+         divErro.setAttribute("id", "erro-div")
+
+    } else {
+        paragrafoErro.computedStyleMap.display = "none"
+         divErro.setAttribute("id", "")
+         pessoas = Numer(evento.target.value)
+
+    }
+
+}
+
+const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
+botoesGorjeta.forEach(botao => {
+    botao.addEventListener("click", receberPorcentagem )
+})
+
+function receberPorcentagem(evento) {
+    botoesGorjeta.forEach(botao => {
+
+        botao.classList.remove("botao-ativo")
+
+        if(botao.value === evento.target.value) {
+            botao.classList.add("botao-ativo")
+        }
+    })
+
+
+
+    porcentagem = Number(evento.target.value) /100
+
+}
